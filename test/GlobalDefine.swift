51c109b9c038c92c280fef900ad6d2b0986382c9
suzee <2469209357@.com>
2016-10-24 18:02:27 +0800

    first commit

---
+++ test/GlobalDefine.swift
@@ -0,0 +1,9 @@
+//
+//  GlobalDefine.swift
+//  test
+//
+//  Created by SuikeTech on 16/10/24.
+//  Copyright © 2016年 SuikeTech. All rights reserved.
+//
+
+import Foundation
---
+++ test/MD5DigestUtil.swift
@@ -0,0 +1,13 @@
+//
+//  MD5DigestUtil.swift
+//  test
+//
+//  Created by SuikeTech on 16/10/24.
+//  Copyright © 2016年 SuikeTech. All rights reserved.
+//
+
+import UIKit
+
+class MD5DigestUtil: NSObject {
+
+}
